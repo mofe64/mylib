deef4b81c43d7b8fbc4bf95ace8456916d495bd9
mofe64 <ogunbiyioladapo33@gmail.com>
2020-04-20 14:30:46 +0100

    author routes and search functionality

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes/indexRoutes");
+const authorRouter = require("./routes/authorsRoutes");
 
 dotenv.config({ path: "./.env" });
 app.set("view engine", "ejs");
@@ -12,7 +14,10 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
