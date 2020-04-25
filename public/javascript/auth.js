b6f0957ec2844de3999414d5b822229e2922a6e5
mofe64 <ogunbiyioladapo33@gmail.com>
2020-04-25 21:58:43 +0100

    minor changes

--- public/javascript/auth.js
+++ public/javascript/auth.js
@@ -14,7 +14,7 @@ const login = async (username, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/user/login',
+      url: '/api/user/login',
       data: {
         username,
         password,
@@ -38,7 +38,7 @@ const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/user/logout',
+      url: '/api/user/logout',
     });
 
     if ((res.data.status = 'success')) {
